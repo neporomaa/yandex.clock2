53624fcb72ed5ade93c3139a8c95006ba0f935c7
neporomaa <romanepomn@gmail.com>
2021-04-15 19:56:04 +0300

    Update helloworld.sh

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #echo "Hello, Yandex!" >> data.txt
-git pull https://github.com/neporomaa/yandex.clock2.git
+#git pull https://github.com/neporomaa/yandex.clock2.git
+git fetch --all
 docker restart nginx
 echo "Hello Yandex!"
